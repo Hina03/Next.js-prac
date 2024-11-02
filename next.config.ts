b0c9d49e7682f68d2ba8d48b1878a4eec8e02b78
Hina03 <hinano.326.20@gmail.com>
2024-11-02 15:37:29 +0900

    11章完

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from 'next';
-
+/** @type {import('next').NextConfig} */
+ 
 const nextConfig = {
   experimental: {
-    ppr: 'increamental',
+    ppr: 'incremental',
   },
 };
-
+ 
 export default nextConfig;
